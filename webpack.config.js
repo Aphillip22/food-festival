8e8d69f9d1ebd63aa97ec6921e2460d99346124c
Aphillip22 <amber.rain.phillips@gmail.com>
2021-12-13 00:48:46 -0700

    redirect script imports to proper bundles

--- webpack.config.js
+++ webpack.config.js
@@ -3,12 +3,17 @@ const webpack = require("webpack");
 const BundleAnalyzerPlugin = require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
 
 module.exports = {
-    entry: './assets/js/script.js',
+    entry: {
+        app: "./assets/js/script.js",
+        events: "./assets/js/events.js",
+        schedule: "./assets/js/schedule.js",
+        tickets: "./assets/js/tickets.js"
+    },
     output: {
         path: path.resolve(__dirname, 'dist'),
-        filename: 'main.bundle.js'
-      },
-      plugins:[
+        filename: '[name].bundle.js'
+    },
+    plugins:[
         new webpack.ProvidePlugin({
           $: "jquery",
           jQuery: "jquery"
@@ -16,6 +21,6 @@ module.exports = {
         new BundleAnalyzerPlugin({
             analyzerMode: "static", // the report outputs to an HTML file in the dist folder
           })
-      ],
-      mode: 'development'
+    ],
+    mode: 'development'
 };
